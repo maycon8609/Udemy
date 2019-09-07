dfb3db749da9a3b5078fb932b568c8e3d500a73b
maycon8609 <maycon8609@gmail.com>
2019-09-07 11:05:10 -0300

    promise

---
+++ webModerno/node/promise.js
@@ -0,0 +1,13 @@
+function falar(seg, text) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(text)
+    }, seg * 1000);
+  })
+}
+
+
+falar(2, 'promise')
+  .then(frase => frase.concat(' in js '))
+  .then(frase => console.log(frase))
+  .catch(err => console.log(err))
